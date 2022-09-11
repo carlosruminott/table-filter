519701d41a1e07994110f3e5d66d5652df7aa0f1
Carlos Ruminott <carlosruminott@gmail.com>
2022-09-11 12:34:26 -0300

    fix: borrado de componentes no usados

--- src/components/table/Row.svelte
+++
@@ -1,9 +0,0 @@
-<tr>
-    <td class="action"></td>
-    <td class="file"></td>
-    <td class="id"></td>
-    <td class="sede"></td>
-    <td class="source"></td>
-    <td class="medium"></td>
-    <td class="campaign"></td>  
-</tr>
