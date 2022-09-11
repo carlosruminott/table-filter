b2cba5db1fb33d122dec90a5bcde3e94decc4f86
Carlos Ruminott <carlosruminott@gmail.com>
2022-09-11 12:35:08 -0300

    feat: componente celda y fetch api

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,13 +1,50 @@
 <script>
-import Table from "./Table.svelte";
-import Row from "./table/Row.svelte";
+import Cell from "./table/Cell.svelte";
+
+let value = ''
+//let response = []
+
+//const handleInput = (event) => value.target.value
+
+
+let response = fetch(`https://rickandmortyapi.com/api/character/`)
+    .then(res => res.json())
+    .then(apiResponse =>  apiResponse.results || [])
+
+// const rowProps = {
+//   action: '',
+//   file: '',
+//   idprodmkt: '',
+//   sede: '',
+//   source: '',
+//   medium: '',
+//   campaign: '',
+// }
+
 </script>
 
 <main>
   <h1>Table filter</h1>
-  <Table>
-    <Row/>
-  </Table>
+  
+  <!-- <input type="text" placeholder="Search" value={value} on:input={handleInput}> -->
+
+  {#await response}
+    <p>Loading...</p>
+  {:then response} 
+    <table>
+      <tbody>
+        {#each response as {id,name,status,gender} }
+          <tr>
+            <Cell cellType="td" text={id} />
+            <Cell cellType="td" text={name} />
+            <Cell cellType="td" text={status} />
+            <Cell cellType="td" text={gender} />
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  {/await}
+
 </main>
 
 <style>
---
+++ src/components/table/Cell.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let cellType = 'td'
+    export let text = ''
+</script>
+
+{#if cellType === 'td'}
+    <td class={text}>{text}</td>
+{:else}
+    <th class={text}>{text}</th>
+{/if}
